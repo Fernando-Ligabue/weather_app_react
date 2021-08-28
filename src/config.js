15c4f8f1eecadc9b01962d58eef21b2a08227347
Fernando-Ligabue <76406175+Fernando-Ligabue@users.noreply.github.com>
2021-08-28 18:46:19 +0100

    Att api calls

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const ConfigKeys = {
+    api_key: process.env.REACT_APP_API_KEY,
+    api_url: process.env.REACT_APP_API_URL,
+    api_img: process.env.REACT_APP_API_IMG
+}
+
+export default ConfigKeys;
